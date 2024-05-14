715d415ef2cef94f7a08bc326c42481543c80f2a
Tuan <mtuann4@gmail.com>
2024-05-15 01:35:53 +0700

    update-2024-05-15

--- run-morning.py
+++ run-morning.py
@@ -2,7 +2,7 @@ import os
 import datetime
 date_str = f"{datetime.datetime.now():%Y-%m-%d}"
 print(date_str)
-update_str = f'"test update-{date_str}"'
+update_str = f'"update-{date_str}"'
 cmds = [
     f'/Users/tuan/miniconda3/envs/ml/bin/python crawl-fl-and-backdoor.py',
     f'git commit -am {update_str}',
